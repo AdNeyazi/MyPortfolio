b2d94ff0966351f078d8a1d44ec071cea31aacb0
Adnan <adnanneyazi88@gmail.com>
2025-02-09 22:08:03 +0530

    first commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,50 @@
+class ProjectsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :set_project, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = current_user.projects.build
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to @project, notice: "Project created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project deleted successfully."
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+end
